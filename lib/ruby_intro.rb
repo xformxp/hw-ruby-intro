ebdd0c3ec344f14bfd36ba8fbe889987948b5e4e
EC2 Default User <ec2-user@ip-172-31-44-239.us-east-2.compute.internal>
2020-01-30 05:23:03 +0000

    Completed part 3, passed all tests

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -23,7 +23,7 @@ end
 
 def hello(name)
   # YOUR CODE HERE
-  return "Hello, " + name
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
@@ -32,12 +32,46 @@ def starts_with_consonant? s
 end
 
 def binary_multiple_of_4? s
-  return /\A[01]*00\Z/ =~ s || "0" == s
   # YOUR CODE HERE
+  return /\A[01]*00\Z/ =~ s || "0" == s
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+def initialize(isbn, price)
+  if isbn == "" || price <= 0
+    raise ArgumentError
+  else
+    @isbn = isbn
+    @price = price
+  end
+end
+
+def isbn()
+  return @isbn
+end
+
+def isbn=(isbn)
+  if isbn == ""
+    raise ArgumentError
+  end
+  @isbn = isbn
+end
+
+def price()
+  return @price
+end
+
+def price=(price)
+  if price <= 0
+    raise ArgumentError
+  end
+  @price = price
+end
+
+def price_as_string()
+  return "$#{format('%.2f', @price)}"
+end
 end
